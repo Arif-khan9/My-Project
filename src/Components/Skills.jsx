3a7bf76dae4cb3a0299f7228e6b951139c411784
Arif-khan9 <ak4040651@gmail.com>
2025-07-31 21:00:05 +0530

    project-complate

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -10,7 +10,7 @@ const Skills = () => {
             <p className='text-xl font-Ovo'>What I offer</p>
             <h3 className='text-5xl mt-3 font-Ovo'>My Skills</h3>
         </div>
-        <div className='flex grid  grid-cols-2 md:grid-cols-3 lg:grid-cols-4  mt-20 gap-15 md:gap-30 place-items-center mx-20'>
+        <div className='flex grid  grid-cols-2 md:grid-cols-3 lg:grid-cols-4  mt-20 gap-15 md:gap-30 place-items-center mx-8'>
            {skillsArr.map((skill , index)=>{
             return(
               <Skill  skill={skill} key={index}/>
