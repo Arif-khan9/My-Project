53db1b7ddbb22d601e0194819ad639cd4f57b606
Arif-khan9 <ak4040651@gmail.com>
2025-08-12 23:25:49 +0530

    issue-solve

--- src/Components/Skill.jsx
+++ src/Components/Skill.jsx
@@ -7,9 +7,9 @@ const Skill = (props) => {
    const {name, src} = props.skill
     
   return (
-   <motion.div className='lex flex-col items-center gap-4  text-xl md:text-2x'variants={fadeIn("down")} initial="hidden" whileInView={"show"}>
+   <motion.div className='flex flex-col items-center gap-4  text-xl md:text-2x' variants={fadeIn("down")} initial="hidden" whileInView={"show"}>
       <img className="  h-[50px] w-[50px] lg:h-[100px]  lg:w-[100px] object-contain "  src={src} width="50px" height="50px" alt="img" /> 
-      <p  className='text-center'>{name}</p>
+      <p>{name}</p>
    </motion.div>
      
   )
